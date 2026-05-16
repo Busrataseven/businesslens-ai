ce6b40ef4c826e0252c75b0f22072c27e910165d
Busrataseven <147428528+Busrataseven@users.noreply.github.com>
2026-05-16 16:15:49 +0300

    Fix CSV encoding issue

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
 
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_csv(uploaded_file, encoding="latin1")
 
     st.success("CSV uploaded successfully!")
 
